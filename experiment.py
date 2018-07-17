d90c3231255e1d8d458aa3d349bca174974e0d13
Josh Cooper <joshuacooper91@yahoo.co.uk>
2018-07-17 17:54:11 +0100

    first commit

---
+++ experiment.py
@@ -0,0 +1,62 @@
+import numpy as np
+from tqdm import tqdm
+
+from itertools import product
+from fractions import Fraction
+
+class Experiment:
+    def __init__(self, probability: Fraction):
+        assert probability.numerator > 0, "Numerator must be positive integer"
+        assert (probability.denominator >= probability.numerator), "Denominator must be at least as large as the numerator"
+        assert (probability.denominator <= 2**10), f"Denominator must be <= {2**10} so calculations are tractable"
+        self._probability = probability
+        self._sequence = ''
+        self._setup_subsequences()
+
+    def _setup_subsequences(self):
+        self._subsequence_length = self._calculate_subsequence_length()
+        self._generate_all_subsequences()
+        self._continuation_subsequences = self._generate_continuation_subsequences()
+        self._termination_subsequences = self._generate_termination_subsequences()
+        self._success_subsequences = self._generate_success_subsequences()
+
+    def _calculate_subsequence_length(self):
+        powers_of_two = [2**n for n in range(100)]
+        subsequence_length = 0
+        while self._probability.denominator > powers_of_two[subsequence_length]:
+            subsequence_length += 1
+        return subsequence_length
+
+    def _generate_all_subsequences(self):
+        subsequences_list = list(product(*[['H', 'T']] * self._subsequence_length))
+        self._subsequences = [''.join(subsequence) for subsequence in subsequences_list]
+
+    def _generate_continuation_subsequences(self):
+        return self._subsequences[self._probability.denominator:]
+
+    def _generate_termination_subsequences(self):
+        return self._subsequences[:self._probability.denominator]
+
+    def _generate_success_subsequences(self):
+        num_success_subsequences = self._probability.numerator
+        return self._subsequences[:num_success_subsequences]
+
+    def _run_episode(self):
+        self._generate_new_sequence()
+        while self._sequence[-self._subsequence_length:] in self._continuation_subsequences:
+            self._generate_new_sequence()
+        success = (self._sequence[-self._subsequence_length:] in (self._success_subsequences))
+        return success
+
+    def _generate_new_sequence(self):
+        toss_outcomes = np.random.choice(['H', 'T'], size=self._subsequence_length)
+        new_subsequence = ''.join(toss_outcomes)
+        self._sequence = self._sequence + new_subsequence
+
+    def run_experiment(self, num_episodes=10000):
+        assert num_episodes > 0 and isinstance(num_episodes, int), 'You must enter a positive integer for num_episodes'
+        results_sequence = []
+        for _ in tqdm(range(num_episodes)):
+            results_sequence.append(self._run_episode())
+        num_successes = sum(results_sequence)
+        return num_successes / num_episodes
---
+++ run.py
@@ -0,0 +1,38 @@
+import argparse
+from fractions import Fraction
+
+from experiment import Experiment
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-n',
+        '--numerator',
+        help='numerator of probability',
+        action='store',
+        required=True,
+        type=int
+    )
+    parser.add_argument(
+        '-d',
+        '--denominator',
+        help='denominator of probability',
+        action='store',
+        required=True,
+        type=int
+    )
+    parser.add_argument(
+        '-e',
+        '--numepisodes',
+        help='number of episodes to run',
+        action='store',
+        type=int
+    )
+
+    args = parser.parse_args()
+
+    experiment = Experiment(Fraction(args.numerator, args.denominator))
+    result = experiment.run_experiment(num_episodes=args.numepisodes)
+    print("Result: {0} from {1:,} episodes".format(result, args.numepisodes))
