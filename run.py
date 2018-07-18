6ec661921286cd1853e0c1e339181f4c7683a7bf
Josh Cooper <joshuacooper91@yahoo.co.uk>
2018-07-18 09:12:27 +0100

    adds true probability to printed results statement

--- run.py
+++ run.py
@@ -4,9 +4,7 @@ from fractions import Fraction
 from experiment import Experiment
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
-
     parser.add_argument(
         '-n',
         '--numerator',
@@ -28,11 +26,12 @@ if __name__ == '__main__':
         '--numepisodes',
         help='number of episodes to run',
         action='store',
-        type=int
+        type=int,
+        default=10000
     )
-
     args = parser.parse_args()
 
     experiment = Experiment(Fraction(args.numerator, args.denominator))
     result = experiment.run_experiment(num_episodes=args.numepisodes)
-    print("Result: {0} from {1:,} episodes".format(result, args.numepisodes))
+    print("Result: {0} from {1:,} episodes. Expected result is {2:.5f}".format(
+        result, args.numepisodes, args.numerator / args.denominator))
